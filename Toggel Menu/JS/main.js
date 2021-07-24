64c7f2903b314844d0140acf29e736cb5fe01667
S.A.M <alexdavid486@gmail.com>
2021-07-24 05:23:43 +0200

    adding a background and improve the js code

--- Toggel Menu/JS/main.js
+++ Toggel Menu/JS/main.js
@@ -1,13 +1,8 @@
 const slideMenu = document.querySelector("#icons");
 const links = document.getElementById("links");
-const icons = document.querySelector("span");
+const section = document.querySelectorAll('a');
 
-slideMenu.addEventListener("click", (e) => {
+slideMenu.addEventListener("click", () => {
     document.body.classList.toggle("active");
     links.style.transition = "all 0.3s ease-in";
-    if (e.target.matches("links")) { 
-        window.onclick = () => {
-        document.body.classList.remove("active");
-        }
-    }
-})
+});
