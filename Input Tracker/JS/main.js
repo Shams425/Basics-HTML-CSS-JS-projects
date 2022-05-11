27377dd5395594b9b6b536abfed4fb3c35b8d032
Shams425 <alexdavid486@gmail.com>
2022-05-11 06:52:24 +0200

    adding letters and words counter

--- Input Tracker/JS/main.js
+++ Input Tracker/JS/main.js
@@ -1,10 +1,14 @@
 const input = document.getElementById("inputText");
 const tracker = document.getElementById("Tracker");
+const lettersCount = document.querySelector(".lettersCount");
+const wordsCount = document.querySelector(".wordsCount");
 
-window.onclick  = () => {
-    input.focus();
-    input.oninput = () => {
-    let value = input.value;
-    tracker.textContent = value.toString();
-    };
-}
+input.oninput = () => {
+  let value = input.value;
+  tracker.textContent = value;
+
+  lettersCount.textContent = value.length;
+
+  let allWords = value.split(" ");
+  wordsCount.textContent = allWords.filter((word) => word != "").length;
+};
